8deb64cba83f01155ce05451ec6b28b2e17634a9
PhilippeDataScienc <philippe.acquier@gmail.com>
2024-02-26 15:12:39 +0100

    modif of imports in specialized_tests.py

--- specialized_test.py
+++ specialized_test.py
@@ -4,6 +4,7 @@ from llama_index.core import VectorStoreIndex, ServiceContext, SimpleDirectoryRe
 from llama_index.llms.ollama import Ollama
 from llama_index.core import StorageContext
 from llama_index.vector_stores.qdrant import QdrantVectorStore
+from llama_index.core import Settings
 
 # Loading the documents from the disk
 documents = SimpleDirectoryReader("./data").load_data()
@@ -17,7 +18,6 @@ storage_context = StorageContext.from_defaults(vector_store=vector_store)
 # The request_timeout may need to be adjusted depending on the system's performance capabilities
 llm = Ollama(model="mixtral", request_timeout=600.0)
 service_context = ServiceContext.from_defaults(llm=llm, embed_model="local")
-
 # Creating the index, which includes embedding the documents into the vector store
 index = VectorStoreIndex.from_documents(documents, service_context=service_context, storage_context=storage_context)
 
